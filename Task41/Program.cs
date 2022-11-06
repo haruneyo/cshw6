cc06f04666669365f6e77ebba826b3e4cdb2d029
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-06 22:36:14 +0500

    Task 41 done

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Specify how many numbers to enter:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+FillArray(array);
+System.Console.WriteLine($"The amount of numbers bigger than zero in the array is: {BiggerThanZero(array)}");
+void FillArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        System.Console.WriteLine($"Enter the {i + 1} number:");
+        a[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int BiggerThanZero(int[] a)
+{
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i] > 0) count++;
+    }
+    return count;
+}
